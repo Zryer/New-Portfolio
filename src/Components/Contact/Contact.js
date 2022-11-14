852634b9a9c85bb67cacb5bcfd4005681420ec57
Zachary Fisher <zacharyfisher808@gmail.com>
2022-11-13 23:59:15 -1000

    fixed navigation and margin issues

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -29,24 +29,24 @@ const Contact = () => {
       <Row>
         <Col xs={12}>
           <Form action="https://formspree.io/f/xjvllrey" method="POST" encType="multipart/form-data" target="_blank" className="contactForm">
-            <Row style={{marginBottom: "2rem"}}>
-            <Form.Group as={Col} xs={6}>
-              <FloatingLabel label="Name">
-                <Form.Control required type="text" id="name" name="name" placeholder="Name" value={formData.name} onChange={(e) => setFormData({ ...formData, name: e.target.value })} />
-              </FloatingLabel>
-            </Form.Group>
-            <Form.Group as={Col} xs={6}>
-              <FloatingLabel label="Email">
-                <Form.Control required type="email" id="email" name="email" placeholder="Enter Email" value={formData.email} onChange={(e) => setFormData({ ...formData, email: e.target.value })} />
-              </FloatingLabel>
-            </Form.Group>
+            <Row style={{ marginBottom: "2rem" }}>
+              <Form.Group as={Col} xs={6}>
+                <FloatingLabel label="Name">
+                  <Form.Control required type="text" id="name" name="name" placeholder="Name" value={formData.name} onChange={(e) => setFormData({ ...formData, name: e.target.value })} />
+                </FloatingLabel>
+              </Form.Group>
+              <Form.Group as={Col} xs={6}>
+                <FloatingLabel label="Email">
+                  <Form.Control required type="email" id="email" name="email" placeholder="Enter Email" value={formData.email} onChange={(e) => setFormData({ ...formData, email: e.target.value })} />
+                </FloatingLabel>
+              </Form.Group>
             </Row>
-            <Row style={{marginBottom: "2rem"}}>
-            <Form.Group as={Col} xs={12}>
-              <FloatingLabel label="Message">
-                <Form.Control required type="text" id="message" name="message" as="textarea" placeholder="Enter Message" value={formData.message} onChange={(e) => setFormData({ ...formData, message: e.target.value })} />
-              </FloatingLabel>
-            </Form.Group>
+            <Row style={{ marginBottom: "2rem" }}>
+              <Form.Group as={Col} xs={12}>
+                <FloatingLabel label="Message">
+                  <Form.Control required type="text" id="message" name="message" as="textarea" placeholder="Enter Message" value={formData.message} onChange={(e) => setFormData({ ...formData, message: e.target.value })} />
+                </FloatingLabel>
+              </Form.Group>
             </Row>
             <Button type="submit" xs={2}>
               Submit
@@ -58,11 +58,11 @@ const Contact = () => {
         </Col>
       </Row>
       <Row>
-      <Col className="contactButtons" style={{ marginBottom: "1rem" }}>
-                    <Button className="homeButton" aria-label="Github" size="lg" style={{ backgroundColor: "#333", border: "none", borderRadius: "50px" }} href="https://github.com/Zryer" target="_blank"><FontAwesomeIcon icon={faGithub} className="buttonIcon" style={{ color: "#FFF", fontSize: "3rem", margin: "0 1rem 0 1rem" }} /></Button>
-                    <Button className="homeButton" aria-label="Linkedin" size="lg" style={{ backgroundColor: "#0A66C2", border: "none", borderRadius: "50px" }} href="https://www.linkedin.com/in/zachary-fisher-1b6448221/" target="_blank"><FontAwesomeIcon icon={faLinkedin} className="buttonIcon" style={{ color: "#FFF", fontSize: "3rem", margin: "0 1rem 0 1rem" }} /></Button>
-                    <Button className="homeButton" aria-label="Email" size="lg" style={{ backgroundColor: "#D5DBDB", border: "none", borderRadius: "50px" }} href="mailto:zacharyfisher808@gmail.com" target="_blank"><FontAwesomeIcon icon={faEnvelopeOpen} className="buttonIcon" style={{ color: "#34495E", fontSize: "3rem", margin: "0 1rem 0 1rem" }} /></Button>
-                </Col>
+        <Col className="contactButtons" style={{ marginBottom: "1rem" }}>
+          <Button className="homeButton" aria-label="Github" size="lg" style={{ backgroundColor: "#333", border: "none", borderRadius: "50px" }} href="https://github.com/Zryer" target="_blank"><FontAwesomeIcon icon={faGithub} className="buttonIcon" style={{ color: "#FFF", fontSize: "3rem", margin: "0 1rem 0 1rem" }} /></Button>
+          <Button className="homeButton" aria-label="Linkedin" size="lg" style={{ backgroundColor: "#0A66C2", border: "none", borderRadius: "50px" }} href="https://www.linkedin.com/in/zachary-fisher-1b6448221/" target="_blank"><FontAwesomeIcon icon={faLinkedin} className="buttonIcon" style={{ color: "#FFF", fontSize: "3rem", margin: "0 1rem 0 1rem" }} /></Button>
+          <Button className="homeButton" aria-label="Email" size="lg" style={{ backgroundColor: "#D5DBDB", border: "none", borderRadius: "50px" }} href="mailto:zacharyfisher808@gmail.com" target="_blank"><FontAwesomeIcon icon={faEnvelopeOpen} className="buttonIcon" style={{ color: "#34495E", fontSize: "3rem", margin: "0 1rem 0 1rem" }} /></Button>
+        </Col>
       </Row>
     </Container>
   )
--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
@@ -9,18 +9,48 @@ import './Navigation.css';
 
 const Navigation = () => {
 
+  useEffect(() => {
+    if(document.documentElement.clientWidth < 991) {
+      console.log(document.documentElement.clientWidth)
+      let navigationHeight = document.querySelector(".myNavbar").offsetHeight;
+      document.documentElement.style.setProperty('--scroll-padding', navigationHeight + 147 + "px")
+    }
+    else {
+      console.log(document.documentElement.clientWidth)
+      let navigationHeight = document.querySelector(".myNavbar").offsetHeight;
+      document.documentElement.style.setProperty('--scroll-padding', navigationHeight - 1 + "px")
+    }
+  })
+
+  window.addEventListener('resize', function() {
+    if(document.documentElement.clientWidth < 991) {
+      console.log(document.documentElement.clientWidth)
+      let navigationHeight = document.querySelector(".myNavbar").offsetHeight;
+      document.documentElement.style.setProperty('--scroll-padding', navigationHeight + 147 + "px")
+    }
+    else {
+      console.log(document.documentElement.clientWidth)
+      let navigationHeight = document.querySelector(".myNavbar").offsetHeight;
+      document.documentElement.style.setProperty('--scroll-padding', navigationHeight - 1 + "px")
+    }
+  })
+
+
+
+
+
   return (
-    <Navbar collapseOnSelect expand="lg" sticky="top" style={{backgroundColor: "#222733"}}>
+    <Navbar className="myNavbar" collapseOnSelect expand="lg" sticky="top" style={{ backgroundColor: "#222733" }}>
       <Container fluid>
-      <Navbar.Brand style={{padding: " 1vw 0 1vw 1vw", cursor: "pointer"}} href="#Home"><Image src={Logo} style={{height: "50px", width: "50px"}} alt="logo" /></Navbar.Brand>
-      <Navbar.Toggle style={{backgroundColor: "#FE6F27"}} />
-      <Navbar.Collapse style={{justifyContent: "flex-end"}}>
-        <Nav style={{padding: " 1vw 1vw 1vw 0"}}>
-        <Nav.Link className="navigationLink" href="#About">About</Nav.Link>
-        <Nav.Link className="navigationLink" href="#Projects">Projects</Nav.Link>
-        <Nav.Link className="navigationLink" href="#Contact">Contact</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
+        <Navbar.Brand style={{ padding: " 1vw 0 1vw 1vw", cursor: "pointer" }} href="#Home"><Image src={Logo} style={{ height: "50px", width: "50px" }} alt="logo" /></Navbar.Brand>
+        <Navbar.Toggle style={{ backgroundColor: "#FE6F27" }} />
+        <Navbar.Collapse style={{ justifyContent: "flex-end" }}>
+          <Nav style={{ padding: " 1vw 1vw 1vw 0" }}>
+            <Nav.Link className="navigationLink" href="#About">About</Nav.Link>
+            <Nav.Link className="navigationLink" href="#Projects">Projects</Nav.Link>
+            <Nav.Link className="navigationLink" href="#Contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   )
